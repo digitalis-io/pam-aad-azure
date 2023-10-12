4ddb9416a957942e0cf38740a51932f8f1dae95f
Sergio Rua <sergio.rua@digitalis.io>
2023-10-12 08:25:07 +0000

    Backup

--- nss/nss.c
+++ nss/nss.c
@@ -4,31 +4,187 @@
 #include <nss.h>
 #include <pwd.h>
 #include <stdio.h>
+#include <sqlite3.h>
+#include <unistd.h>
+#include <string.h>
+
+#define DB_FILE_NAME "azure.db"
+#define DEBUG 1
+
+sqlite3 *db_connect() {
+    sqlite3 *db;
+    sqlite3_stmt *res;
+    const char *cache_directory = "/tmp";
+    char db_path[strlen(cache_directory)+strlen(DB_FILE_NAME)];
+    char *err_msg = 0;
+
+    sprintf(db_path, "%s/%s", cache_directory, DB_FILE_NAME);
+    if (access(db_path, F_OK) != 0) {
+        fprintf(stderr,  "Cannot connect to the database because it has not been initialised");
+        return NULL;
+    }
+
+    int rc = sqlite3_open(db_path, &db);
+    
+    if (rc != SQLITE_OK) {
+        fprintf(stderr,  "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        
+        return NULL;
+    } 
+    return db;
+}
+
+enum nss_status get_group_by_query(const char *query, struct group *result) {
+    sqlite3 *db;
+    sqlite3_stmt *res;
+    int rc;
+    char *err_msg = 0;
+
+    db = db_connect();
+    if (db == NULL)
+        return NSS_STATUS_TRYAGAIN;
+
+    if (DEBUG)
+        fprintf(stderr, "==>> %s\n", query);
+
+    rc = sqlite3_prepare_v2(db, query, -1, &res, 0);    
+    
+    if (rc != SQLITE_OK) {
+        sqlite3_close(db);
+        
+        return NSS_STATUS_UNAVAIL;
+    }
+
+    /* init struct with random default */
+    *result = (struct group) {
+        .gr_passwd = "x"
+    };
+    // Execute the SQL statement and fetch the results
+    while ((rc = sqlite3_step(res)) == SQLITE_ROW) {
+        // Access column values using sqlite3_column_* functions
+        result->gr_name = strdup(sqlite3_column_text(res, 0));
+        result->gr_gid = sqlite3_column_int(res, 1);
+    }
+    
+    if (!result->gr_gid)
+        rc = NSS_STATUS_UNAVAIL;
+    else
+        rc = NSS_STATUS_SUCCESS;
+
+    sqlite3_finalize(res);
+    sqlite3_close(db);
+    return rc;
+}
+
+enum nss_status get_user_by_query(const char *query, struct passwd *result) {
+    sqlite3 *db;
+    sqlite3_stmt *res;
+    int rc;
+    char *err_msg = 0;
+
+    db = db_connect();
+    if (db == NULL)
+        return NSS_STATUS_TRYAGAIN;
+
+    if (DEBUG)
+        fprintf(stderr, "==>> %s\n", query);
+
+    rc = sqlite3_prepare_v2(db, query, -1, &res, 0);    
+    
+    if (rc != SQLITE_OK) {
+        sqlite3_close(db);
+        
+        return NSS_STATUS_UNAVAIL;
+    }
+
+    /* init struct with random default */
+    *result = (struct passwd) {
+        .pw_shell = "/bin/bash"
+    };
+    // Execute the SQL statement and fetch the results
+    while ((rc = sqlite3_step(res)) == SQLITE_ROW) {
+        // Access column values using sqlite3_column_* functions
+        result->pw_name = strdup(sqlite3_column_text(res, 0));
+        result->pw_uid = sqlite3_column_int(res, 1);
+        result->pw_gid = sqlite3_column_int(res, 2);
+        result->pw_gecos = strdup(sqlite3_column_text(res, 3));
+        result->pw_dir = strdup(sqlite3_column_text(res, 4));
+        result->pw_shell = strdup(sqlite3_column_text(res, 5));
+    }
+    
+    if (!result->pw_uid)
+        rc = NSS_STATUS_UNAVAIL;
+    else
+        rc = NSS_STATUS_SUCCESS;
+
+    sqlite3_finalize(res);
+    sqlite3_close(db);
+    return rc;
+}
+
+/**********************************************************/
 
 enum nss_status _nss_aad_setpwent (void) {
-    fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
+    if (DEBUG) fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
     return NSS_STATUS_SUCCESS;
 }
+
 enum nss_status _nss_aad_endpwent (void) {
-    fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
+    if (DEBUG) fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
     return NSS_STATUS_SUCCESS;
 }
-enum nss_status _nss_aad_getpwnam_r (struct passwd *result, char *buffer, size_t buflen, int *errnop) {
-    fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
-    result->pw_name = "sergio";
-    result->pw_gecos = "Serg";
-    result->pw_gid = 900000;
-    result->pw_uid = 999999;
-    result->pw_shell = "/bin/bash";
-    return NSS_STATUS_SUCCESS;
+
+enum nss_status _nss_aad_getpwnam_r (const char *name, struct passwd *result, char *buffer, size_t buflen, int *errnop) {
+    if (DEBUG) fprintf(stderr, "NSS DEBUG: Called %s with arguments name = %s buffer = %s\n", __FUNCTION__, name, buffer);
+    char query[255];
+    sprintf(query, "SELECT login, uid, gid, gecos, home, shell FROM passwd WHERE login = '%s'", name);
+
+    int rc = get_user_by_query((char *)query, result);
+
+    return rc;
 }
 
 enum nss_status _nss_aad_getpwbyuid_r (uid_t uid, struct passwd *result, char *buffer, size_t buflen, int *errnop) {
-    fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
-    return NSS_STATUS_SUCCESS;
+    if (DEBUG) fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
+    char query[255];
+    sprintf(query, "SELECT login, uid, gid, gecos, home, shell FROM passwd WHERE uid = %d", uid);
+
+    int rc = get_user_by_query((char *)query, result);
+
+    return rc;
 }
 
 enum nss_status _nss_aad_getpwbynam_r (const char *name, struct passwd *result, char *buffer, size_t buflen, int *errnop) {
-    fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
+    if (DEBUG) fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
+    return NSS_STATUS_SUCCESS;
+}
+
+enum nss_status _nss_aad_getspnam_r (gid_t gid, struct group *gr, char *buffer, size_t buflen, int *errnop) {
+    if (DEBUG) fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
+    return NSS_STATUS_SUCCESS;
+}
+
+/* Groups */
+
+/*
+ * getgrnam
+ */
+
+enum nss_status _nss_aad_getgrnam_r(const char *name, struct group *gr, char *buffer, size_t buflen, int *errnop) {
+    if (DEBUG) fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
+    char query[255];
+    sprintf(query, "SELECT name, gid FROM groups WHERE name = '%s'", name);
+
+    int rc = get_group_by_query((char *)query, gr);
+
+    return rc;
+}
+
+enum nss_status _nss_aad_getgrent_r(
+                struct group *result,
+                char *buffer, size_t buflen,
+                int *errnop) {
+    if (DEBUG) fprintf(stderr, "NSS DEBUG: Called %s\n", __FUNCTION__);
     return NSS_STATUS_SUCCESS;
 }
