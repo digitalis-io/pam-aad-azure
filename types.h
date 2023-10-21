6e66fbbcceb0a45aed4628dacd14f83ba1980b58
Sergio Rua <sergio.rua@digitalis.io>
2023-10-21 17:53:04 +0000

    Backup

---
+++ types.h
@@ -0,0 +1,36 @@
+#include <jansson.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#define HOME_ROOT "/home"
+#define CONFIG_FILE "/etc/pam_aad.conf"
+#define AUTH_ERROR "authorization_pending"
+#define HOST "https://login.microsoftonline.com/"
+#define SCOPE "https%3A%2F%2Fgraph.microsoft.com%2F.default+openid+profile+email"
+#define GRAPH "https://graph.microsoft.com/v1.0"
+#define TTW 5                   /* time to wait in seconds */
+#define USER_AGENT "azure_authenticator_pam/1.0"
+
+struct nss_config {
+    char *cache_directory;
+    char *cache_owner; 
+    char *cache_group;
+    char *cache_mode;
+    char *client_id;
+    char *group_id;
+    char *group_name;
+    char *tenant;
+    char *client_secret;
+    char *domain;
+    char *ab_token;
+    char *home_directory;
+    bool debug;
+};
+
+int load_config(struct nss_config *json_config);
